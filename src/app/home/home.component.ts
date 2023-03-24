cb35ddebd75c790a606d1854dcd5d80cdbec4208
kiranm <kiranm@winjit.com>
2023-03-24 17:35:21 +0530

    commit changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -56,12 +56,12 @@ export class HomeComponent implements OnInit {
 
   async getCustomerList() {
     var colData = collection(this.firbaseService.db, 'Customer');
-    const q = query(
-      colData,
-      where('isActive', '==', true),
-      where('externalCust', '==', false)
-    );
-    const citySnapshot = await getDocs(q);
+    // const q = query(
+    //   colData,
+    //   where('isActive', '==', true),
+    //   where('externalCust', '==', false)
+    // );
+    const citySnapshot = await getDocs(colData);
     var customerList: any = citySnapshot.docs.map((doc) => doc.data());
     this.totalCustomer = customerList.length;
   }
